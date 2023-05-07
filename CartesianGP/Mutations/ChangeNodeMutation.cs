93146801e13e3efdfbd8bf4c5b746296a5a77e5b
Marek Nagy <62247071@cuni.cz>
2023-05-07 12:28:16 +0200

    [WIP] implementing mutation

--- CartesianGP/Mutations/ChangeNodeMutation.cs
+++ CartesianGP/Mutations/ChangeNodeMutation.cs
@@ -2,14 +2,16 @@ public class ChangeNodeMutation : Mutation<CartesianChromosome>
 {
     private Random _rng = new Random();
     public double PercentageToChange { get; }
-    public ChangeNodeMutation(double chromosomePercentageToChange, double probability): base(probability)
+    private readonly Dictionary<int, List<CartesianNode>> NodeCatalogue;
+    public ChangeNodeMutation(double chromosomePercentageToChange, double probability, Dictionary<int, List<CartesianNode>> nodeCatalogue): base(probability)
     {
         this.PercentageToChange = chromosomePercentageToChange;
+        this.NodeCatalogue = nodeCatalogue;
     }
     public override CartesianChromosome Mutate(CartesianChromosome ind, int genNum)
     {
         double rand_value;
-        lock(this)
+        lock(_rng)
         {
             rand_value = _rng.NextDouble();
         }
@@ -19,6 +21,14 @@ public class ChangeNodeMutation : Mutation<CartesianChromosome>
             return ind.Clone();
         
         // mutate
-        throw new NotImplementedException();
+        List<List<double>> node_probabilities;
+        lock(_rng)
+        {
+            // TODO: continue here with NodeCatalogue
+            node_probabilities = ind.GetLayerSizes()
+                .Select(layerSize => Enumerable.Range(0, layerSize)
+                    .Select(_ => _rng.)
+                )
+        }
     }
 }
