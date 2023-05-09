b00e0c0f6bdc2ac250d188674c0680a109865e81
Marek Nagy <62247071@cuni.cz>
2023-05-09 13:54:48 +0200

    Finished mutation implementation

--- CartesianGP/Mutations/ChangeNodeMutation.cs
+++ CartesianGP/Mutations/ChangeNodeMutation.cs
@@ -2,11 +2,15 @@ public class ChangeNodeMutation : Mutation<CartesianChromosome>
 {
     private Random _rng = new Random();
     public double PercentageToChange { get; }
-    private readonly Dictionary<int, List<CartesianNode>> NodeCatalogue;
-    public ChangeNodeMutation(double chromosomePercentageToChange, double probability, Dictionary<int, List<CartesianNode>> nodeCatalogue): base(probability)
+    private readonly IReadOnlyDictionary<int, IList<CartesianNode>> NodeCatalogue;
+    private readonly IList<CartesianNode> Nodes;
+    public ChangeNodeMutation(double chromosomePercentageToChange, double probability, Dictionary<int, IList<CartesianNode>> nodeCatalogue): base(probability)
     {
         this.PercentageToChange = chromosomePercentageToChange;
         this.NodeCatalogue = nodeCatalogue;
+        this.Nodes = nodeCatalogue.Keys
+            .SelectMany(arity => nodeCatalogue[arity])
+            .ToArray();
     }
     public override CartesianChromosome Mutate(CartesianChromosome ind, int genNum)
     {
@@ -21,14 +25,34 @@ public class ChangeNodeMutation : Mutation<CartesianChromosome>
             return ind.Clone();
         
         // mutate
-        List<List<double>> node_probabilities;
+        IList<IList<double>> nodeProbabilities;
         lock(_rng)
         {
-            // TODO: continue here with NodeCatalogue
-            node_probabilities = ind.GetLayerSizes()
+            nodeProbabilities = ind.GetLayerSizes()
                 .Select(layerSize => Enumerable.Range(0, layerSize)
-                    .Select(_ => _rng.)
+                    .Select(_ => _rng.NextDouble())
+                    .ToArray()
                 )
+                .ToArray();
         }
+
+        // TODO: replace node with probability < this.PercentageToChange
+        var layers = ind.DeepCopyLayers();
+        for (int i = 0; i < layers.Count; i++)
+        {
+            for (int j = 0; j < layers[i].Count; j++)
+            {
+                if ( nodeProbabilities[i][j] < this.PercentageToChange )
+                {
+                    // choose new random node, preserve parents
+                    layers[i][j] = _rng.Choose<CartesianNode>(this.Nodes).Clone(layers[i][j].Parents);
+                }
+            }
+        }
+
+        return new CartesianChromosome(
+            ind.InputsAmount,
+            layers
+        );
     }
 }
