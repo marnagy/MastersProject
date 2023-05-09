c5ba7008df1720ab1912463fb3bf479287bc3757
Marek Nagy <62247071@cuni.cz>
2023-05-09 17:05:07 +0200

    Renamed combination

--- CartesianGP/PopulationCombinationStrategies/TakeNewCombination.cs
+++ CartesianGP/PopulationCombinationStrategies/TakeNewCombination.cs
@@ -1,4 +1,4 @@
-public class DummyCombination : PopulationCombinationStrategy<CartesianChromosome>
+public class TakeNewCombination : PopulationCombinationStrategy<CartesianChromosome>
 {
     public override CartesianChromosome[] Combine(CartesianChromosome[] oldPopulation, CartesianChromosome[] newPopulation)
         => newPopulation;
