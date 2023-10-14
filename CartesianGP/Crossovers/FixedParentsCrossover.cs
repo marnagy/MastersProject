29f67ab696a639d34062e13c5dc958ae01103520
Marek Nagy <62247071@cuni.cz>
2023-10-14 20:18:08 +0200

    [WIP] Adding Crossovers for CartesianGP

---
+++ CartesianGP/Crossovers/FixedParentsCrossover.cs
@@ -0,0 +1,8 @@
+public class FixedParentsCrossover : Crossover<CartesianChromosome>
+{
+    public override Tuple<CartesianChromosome, CartesianChromosome> Cross(CartesianChromosome ind1, CartesianChromosome ind2)
+        => new Tuple<CartesianChromosome, CartesianChromosome>(
+            ind1.Clone(),
+            ind2.Clone()
+        );
+}
