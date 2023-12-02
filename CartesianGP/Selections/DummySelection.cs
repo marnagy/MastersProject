cfe38af7e0455c1552c8163792132a1d3394b09b
Marek Nagy <62247071@cuni.cz>
2023-12-02 14:33:48 +0100

    Updated Selection argument type

--- CartesianGP/Selections/DummySelection.cs
+++ CartesianGP/Selections/DummySelection.cs
@@ -1,7 +1,7 @@
 public class DummySelection : Selection<CartesianChromosome>
 {
     private Random _rng = new Random();
-    public override Tuple<CartesianChromosome, CartesianChromosome> ChooseParents(IList<CartesianChromosome> population)
+    public override Tuple<CartesianChromosome, CartesianChromosome> ChooseParents(IReadOnlyList<CartesianChromosome> population)
         => new Tuple<CartesianChromosome, CartesianChromosome>(
             population[_rng.Next(population.Count)].Clone(),
             population[_rng.Next(population.Count)].Clone()
