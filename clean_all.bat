92f28f33d504a5e5e6b621ad554f69c380b63103
Marek Nagy <62247071@cuni.cz>
2023-12-08 01:52:04 +0100

    Added TreeBasedGP to .bat

--- clean_all.bat
+++ clean_all.bat
@@ -4,3 +4,7 @@ call rmdir GASharp\bin /S /Q
 call rmdir GASharp\obj /S /Q
 call rmdir CartesianGP\bin /S /Q
 call rmdir CartesianGP\obj /S /Q
+call rmdir HelperClasses\bin /S /Q
+call rmdir HelperClasses\obj /S /Q
+call rmdir TreeBasedGP\bin /S /Q
+call rmdir TreeBasedGP\obj /S /Q
