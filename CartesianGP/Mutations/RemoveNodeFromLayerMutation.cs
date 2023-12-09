84834f04783e78df54d8200e926642abe4545762
Marek Nagy <62247071@cuni.cz>
2023-12-09 13:07:16 +0100

    Added RemoveNodeFramLayerMutation

---
+++ CartesianGP/Mutations/RemoveNodeFromLayerMutation.cs
@@ -0,0 +1,76 @@
+public class RemoveNodeFromLayerMutation : Mutation<CartesianChromosome>
+{
+    public RemoveNodeFromLayerMutation(double probability, int? seed): base(probability, seed) { }
+    public override CartesianChromosome Mutate(CartesianChromosome ind, int genNum)
+    {
+        double rand_value;
+        lock (_rng)
+        {
+            rand_value = _rng.NextDouble();
+        }
+
+        // don't mutate
+        if (rand_value > this.MutationProbability)
+            return ind.Clone();
+        
+        var layers = ind.DeepCopyLayers();
+        
+        int layerToRemoveNodeFrom;
+        int indexOfNodeToRemove;
+        lock (this)
+        {   
+            // don't remove from output layer
+            layerToRemoveNodeFrom = this._rng.Next(layers.Count - 1);
+            indexOfNodeToRemove = this._rng.Next(layers[layerToRemoveNodeFrom].Count);
+        }
+
+        // remove node
+        layers[layerToRemoveNodeFrom].RemoveAt(indexOfNodeToRemove);
+
+        // TODO: fix any indices in layers after chosen node
+        foreach (var layer in layers[(layerToRemoveNodeFrom+1)..])
+        {
+            foreach (var node in layer)
+            {
+                var newParents = node.Parents
+                    .Select(parent => {
+                        // node points to different layer
+                        // LayerIndex includes input layer
+                        if (parent.LayerIndex != layerToRemoveNodeFrom + 1)
+                            return parent.Clone();
+                        else if (parent.Index < indexOfNodeToRemove)
+                            return parent.Clone();
+                        else if (parent.Index > indexOfNodeToRemove)
+                            return new ParentIndices(){
+                                LayerIndex=parent.LayerIndex,
+                                Index=parent.Index-1
+                            };
+                        else // choose new random parent
+                        {
+                            int newLayerIndex;
+                            int newNodeIndex; 
+                            lock (this)
+                            {
+                                newLayerIndex = this._rng.Next(layerToRemoveNodeFrom + 1);
+                                newNodeIndex = this._rng.Next(ind[newLayerIndex].Count);
+                            }
+                            return new ParentIndices(){
+                                LayerIndex=newLayerIndex,
+                                Index=newNodeIndex
+                            };
+                        }
+                    })
+                    .ToArray();
+                
+                node.Parents = newParents;
+            }
+        }
+
+        var newChromosome = new CartesianChromosome(ind.InputsAmount, layers);
+
+        if (!newChromosome.IsValid())
+            throw new Exception($"Created invalid choromosome in {this.GetType()}");
+        
+        return newChromosome;
+    }
+}
