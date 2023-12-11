ab7f5d3d285033031649a87c3a8f25b44f13fbc4
Marek Nagy <62247071@cuni.cz>
2023-12-12 00:34:41 +0100

    [WIP] Preparing for testing

--- TreeBasedGP/Options.cs
+++ TreeBasedGP/Options.cs
@@ -2,16 +2,27 @@ using CommandLine;
 
 public class Options
 {
+    // general
     [Option("single-threaded", Default = true, Required = false, Group = "threads", HelpText = "Run GP in single-thread.")]
     public bool SingleThreaded { get; set; }
     [Option("multi-threaded", Default = false, Required = false, Group = "threads", HelpText = "Run GP in multiple threads.")]
     public bool MultiThreaded { get; set; }
     [Option("json", Default = null, HelpText = "Input JSON for easier loading of hyperparameters.")]
     public string? JsonFilePath { get; set; }
+    [Option("input-csv", Required = true, HelpText = "CSV of input values.")]
+    public string CSVFilePath { get; set; }
+    [Option("input-csv-inputs-amount", Required = true, HelpText = "Amount of input columns in CSV file.")]
+    public int CSVInputsAmount { get; set; }
+    [Option("input-csv-delimiter", Default = ',', HelpText = "Delimiter")]
+    public char CSVDelimiter { get; set; }
     [Option("min-threads", Default = 2, /*Min = 1, Max = 32,*/ HelpText = "Minimum amount of threads to be used by ThreadPool class.")]
     public int MinThreads { get; set; }
     [Option("max-threads", Default = 4, /*Min = 1, Max = 32,*/ HelpText = "Maximum amount of threads to be used by ThreadPool class.")]
     public int MaxThreads { get; set; }
     [Option("seed", HelpText = "Seed for the random number generator used in the GP algorithm.")]
     public int? Seed { get; set; } = null;
-}   
+
+    // specific for TreeBasedGP
+    [Option("terminal-nodes-probability", Default = 0.2d, HelpText = "Probability of choosing from terminal nodes instead of non-terminal nodes.")]
+    public double TerminalNodesProbability { get; set; }
+}
--- TreeBasedGP/Program.cs
+++ TreeBasedGP/Program.cs
@@ -1,35 +1,99 @@
-﻿double terminalNodesProbability = 0.2d;
-var inputNodes = Enumerable.Range(0, 2)
-    .Select(i => new InputNode(1d + i, inputIndex: i))
-    .ToArray();
-var terminalNodesProbabilities = new Dictionary<TreeNode, double> {
-    {new ValueNode(0d, null), 0.3d},
-    {inputNodes[0], 0.3d},
-    {inputNodes[1], 0.3d},
-};
-var nonTerminalNodesProbabilities = new Dictionary<TreeNode, double> {
-    {new SumNode(
-        children: [inputNodes[1], inputNodes[0], new ValueNode(3d, null)]
-        ), 0.4d}
-};
-int? seed = null;
+﻿using CommandLine;
+using Microsoft.Win32.SafeHandles;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Options cliArgs = Parser.Default.ParseArguments<Options>(args).Value;
+
+        double terminalNodesProbability = cliArgs.TerminalNodesProbability;
+
+        // prepare CSV
+        (var inputs, var outputs) = PrepareCSV(
+            cliArgs.CSVFilePath,
+            cliArgs.CSVInputsAmount,
+            cliArgs.CSVDelimiter
+        );
+
+        var inputNodes = Enumerable.Range(0, 2)
+            .Select(i => new InputNode(1d + i, inputIndex: i))
+            .ToArray();
+        var terminalNodesProbabilities = new Dictionary<TreeNode, double> {
+            {new ValueNode(0d, null), 0.3d},
+            {inputNodes[0], 0.3d},
+            {inputNodes[1], 0.3d},
+        };
+        var nonTerminalNodesProbabilities = new Dictionary<TreeNode, double> {
+            {new SumNode(
+                children: [inputNodes[1], inputNodes[0], new ValueNode(3d, null)]
+                ), 0.4d}
+        };
+        int? seed = null;
 
 
-// how to handle setting inputs as terminals?
-// for each input, update terminalNodesProbabilities dictionary (remove old inputs, add new ones)
-TreeChromosome baseChromosome = new TreeChromosome(
-    new SumNode(
-        children: [inputNodes[1], inputNodes[0], new ValueNode(3d, null)]
-        ),
-    terminalNodesProbability,
-    terminalNodesProbabilities,
-    nonTerminalNodesProbabilities,
-    seed
-);
+        // how to handle setting inputs as terminals?
+        // for each input, update terminalNodesProbabilities dictionary (remove old inputs, add new ones)
+        TreeChromosome baseChromosome = new TreeChromosome(
+            new SumNode(
+                children: [inputNodes[1], inputNodes[0], new ValueNode(3d, null)]
+                ),
+            terminalNodesProbability,
+            terminalNodesProbabilities,
+            nonTerminalNodesProbabilities,
+            seed
+        );
 
-System.Console.WriteLine(baseChromosome);
+        System.Console.WriteLine(baseChromosome);
 
-inputNodes[0].Update(5d);
-inputNodes[1].Update(20d);
+        inputNodes[0].Update(5d);
+        inputNodes[1].Update(20d);
 
-System.Console.WriteLine(baseChromosome);
+        System.Console.WriteLine(baseChromosome);
+    }
+    public static (IReadOnlyList<double[]>, IReadOnlyList<double[]>) PrepareCSV(
+            string CSVFilePath, int inputColumnsAmount,
+            char delimiter
+        )
+    {
+        var inputs = new List<double[]>();
+        var outputs = new List<double[]>();
+        using ( var sr = new StreamReader(File.OpenRead(CSVFilePath)))
+        {
+            string? line;
+            int linePartsAmount = -1;
+            IList<string> lineParts;
+            bool columnNamesLine = true;
+            while ( (line = sr.ReadLine()) != null)
+            {
+                if (columnNamesLine)
+                {
+                    linePartsAmount = line.Split(delimiter).Length;
+                    columnNamesLine = false;
+                    continue;
+                }
+
+                lineParts = line.Split(delimiter);
+                if (lineParts.Count != linePartsAmount)
+                    throw new Exception($"Invalid CSV: Expected amount of values {linePartsAmount}, actual {lineParts.Count}");
+                
+                inputs.Add(
+                    Enumerable.Range(0, inputColumnsAmount)
+                        .Select(i => double.TryParse(lineParts[i], out double res)
+                            ? res
+                            : throw new Exception($"Invalid CSV format: Expected double, found {lineParts[i]}") )
+                        .ToArray()
+                );
+                outputs.Add(
+                    Enumerable.Range(inputColumnsAmount, linePartsAmount - inputColumnsAmount)
+                        .Select(i => double.TryParse(lineParts[i], out double res)
+                            ? res
+                            : throw new Exception($"Invalid CSV format: Expected double, found {lineParts[i]}") )
+                        .ToArray()
+                );
+            }
+        }
+
+        return (inputs, outputs);
+    }
+}
