c275573d52e1ccd84f16089a3025e5beafa2aa55
Marek Nagy <62247071@cuni.cz>
2023-12-14 21:56:07 +0100

    Updated python script

--- python_scripts/prepare_input.py
+++ python_scripts/prepare_input.py
@@ -1,13 +1,21 @@
 from argparse import Namespace, ArgumentParser
 import pandas as pd
 import numpy as np
+from typing import List
 
 def get_args() -> Namespace:
     parser = ArgumentParser()
 
     parser.add_argument(type=str, help='Filepath to csv file.', dest='filepath')
     parser.add_argument('-d', '--delimiter', default=',')
-    parser.add_argument('--one-hot-output', action='store_true', default=False)
+    parser.add_argument('--index-col', help='If CSV has index column, please include its name here.')
+    parser.add_argument('--one-hot', action='store_true', default=False,
+                        help='Convert all categorical columns to one-hot encoding,'
+                        ' else convert to range 1-[number of categories]')
+    parser.add_argument('--include-index', action='store_true', default=False,
+                        help='Include index in final CSV file.')
+
+    parser.description = 'Script assumes last column is the output column.'
 
     return parser.parse_args()
 
@@ -15,23 +23,36 @@ def main() -> None:
     args = get_args()
     print(args)
     
-    df = pd.read_csv(args.filepath, delimiter=args.delimiter)
+    df = pd.read_csv(args.filepath, delimiter=args.delimiter, index_col='Id')
 
-    if args.one_hot_output:
-        one_hot_outputs = pd.get_dummies(df['Species'])
-        for col in one_hot_outputs.columns:
-            df.insert(len(df.columns), col, one_hot_outputs[col])
-            df[col] = df[col].astype(np.float64)
-
-        df.drop(['Id', 'Species'], axis='columns', inplace=True)
-    else:
-        # TODO: store class id in 'Species' column
-        df.drop(['Id'], axis='columns', inplace=True)
+    column_index = 0
+    last_column_converted_to_columns_amount = 1
+    while column_index < len(df.columns):
+        column_name = df.columns[column_index]
+        if df.dtypes[column_name] == object: # categorical
+            if args.one_hot:
+                one_hot_encoded_column = pd.get_dummies(df[column_name])
+                df.drop(columns=[column_name], inplace=True)
+                for col_i, col in enumerate(one_hot_encoded_column.columns):
+                    new_column_name = f'{column_name}-{col_i}'
+                    df.insert(column_index + col_i, new_column_name, one_hot_encoded_column[col])
+                    df[new_column_name] = df[new_column_name].astype(np.int64)
+                column_index += len(one_hot_encoded_column.columns)
+                last_column_converted_to_columns_amount = len(one_hot_encoded_column.columns)
+            else:
+                values: List[str] = df[column_name].unique().tolist()
+                df.insert(column_index, f'{column_name}-converted', df[column_name].apply(lambda val: values.index(val)))
+                df.drop(columns=[column_name], inplace=True)
+                column_index += 1
+                last_column_converted_to_columns_amount = 1
+        else:
+            column_index += 1
+            last_column_converted_to_columns_amount = 1
     
+    print(df.head(5))
+    print(f'Input columns: {len(df.columns) - last_column_converted_to_columns_amount}, output_columns: {last_column_converted_to_columns_amount}')
 
-    print(df)
-
-    df.to_csv(f'prepared_{args.filepath}', sep=args.delimiter, index=False, )
+    df.to_csv(f'prepared_{args.filepath}', sep=args.delimiter, index=args.include_index)
 
 if __name__ == '__main__':
     main()
